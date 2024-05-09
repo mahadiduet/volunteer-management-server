5396e0fd62f3e5d0a330f6a34112061422700afc
mahadiduet <mahadiduet@gmail.com>
2024-05-10 01:30:08 +0600

    project setup in server site

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) =>{
+    res.send('Server running......');
+})
+
+app.listen(port, ()=>{
+    console.log(`Server running port ${port}`);
+})
