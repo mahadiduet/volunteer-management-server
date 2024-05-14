02acb78981a9e5f10d29fe05171f484baf5373b1
mahadiduet <mahadiduet@gmail.com>
2024-05-14 13:52:53 +0600

    some error fix

--- index.js
+++ index.js
@@ -7,14 +7,23 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: [
-    // 'http://localhost:5173',
-    'https://volunteer-management-b22ec.web.app',
-    'https://volunteer-management-b22ec.firebaseapp.com'
-  ],
-  credentials: true
-}));
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://volunteer-management-b22ec.web.app",
+      "https://volunteer-management-b22ec.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 
 app.get('/', (req, res) => {
   res.send('Server running......');
@@ -55,11 +64,6 @@ const verifyToken = (req, res, next) => {
   })
 }
 
-const cookieOptions = {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-};
 
 async function run() {
   try {
@@ -77,7 +81,7 @@ async function run() {
     })
 
     app.post('/logout', async (req, res) => {
-      const user = req.body;
+      // const user = req.body;
       res.clearCookie('token', { ...cookieOptions, maxAge: 0 }).send({ success: true })
     })
 
