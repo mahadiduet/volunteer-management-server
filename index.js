5fd1b763396c2db018f1955accb98cffc58b6aee
mahadiduet <mahadiduet@gmail.com>
2024-05-15 23:46:30 +0600

    jwt try ctach function add

--- index.js
+++ index.js
@@ -92,8 +92,7 @@ async function run() {
     })
 
     app.post('/logout', async (req, res) => {
-      // const user = req.body;
-      // res.clearCookie('token', { ...cookieOptions, maxAge: 0 }).send({ success: true })
+
       const user = req.body
       res.clearCookie('token', {
           maxAge: 0,
